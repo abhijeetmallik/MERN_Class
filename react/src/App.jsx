3e0ddcdae7b40234cf3427e1f14c41fe1be6275f
Abhijeet Mallik <abhijeetmallick3125@gmail.com>
2025-01-25 13:44:53 +0530

    2nd commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Timer from './components/Timer'
 import WorkTimer from './components/WorkTimer'
 
 const App = () => {
   return (
     <div className='flex justify-center items-center  w-[100%] border-[1px] border-[black] h-[100vh]'> 
-      {/* <Timer /> */}
       <WorkTimer />
     </div>
   )
